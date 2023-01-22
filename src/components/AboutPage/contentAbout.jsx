4bc81998c7f7b90debec6b9c7bc63e9dbff7caed
David Raj Micheal <daviddgl2013@gmail.com>
2023-01-22 20:56:16 +0530

    updated brochure and some content

--- src/components/AboutPage/contentAbout.jsx
+++ src/components/AboutPage/contentAbout.jsx
@@ -3,27 +3,26 @@ import React from "react";
 export default function ContentAbout() {
   return (
     <div className="content-container">
-
       <div className="contentAbout" id="ContentAbout">
         <p className="about-para1">
-          LINEAR ALGEBRA IS UNDENIABLY THE BASE FOR MANY FIELDS OF SCIENCES SUCH
-          AS MACHINE LEARNING, DEEP LEARNING, NEURAL NETWORKS, SUPPLY CHAIN
-          MANAGEMENT, DIGITAL SIGNAL PROCESSING, PATTERN RECOGNITION, TO NAME A
-          FEW. THUS IT HAS BECOME MANDATORY FOR RESEARCHERS AND FACULTIES TO
-          REVISE AND BE UPDATED WITH THIS SUBJECT.
-
-
+          Linear algebra is undeniably the base for many fields of sciences such
+          as Machine Learning, Deep Learning, Neural Networks, Supply Chain
+          Management, Digital Signal Processing, Pattern Recognition, to name a
+          few. Thus it has become mandatory for researchers and faculties to
+          revise and be updated with this subject.
         </p>
 
-
         <p className="about-para2">
-          THIS FDP IS MAINLY FOCUSED ON BRUSHING UP THE BASICS AS WELL AS BRINGING
-          OUT THE RESEARCH BEING CARRIED OUT AROUND THE GLOBE ABOUT THE
-          APPLICATIONS IN DIFFERENT DOMAINS. THIS FDP WILL INTRODUCE THE BASICS OF
-          LINEAR ALGEBRA AT A POST GRADUATE LEVEL AND ALSO SHOWCASE APPLICATIONS
-          CURRENTLY BEING RESEARCHED.
-
-
+          This FDP is mainly focused on brushing up the basics as well as
+          bringing out the research being carried out around the globe about the
+          applications in different domains. This FDP will introduce the basics
+          of Linear Algebra at a post graduate level and also showcase
+          applications currently being researched.
+        </p>
+        <p className="about-para1">
+          Both research scholars and faculties working in the linear algebra and
+          its allied areas are welcome to participate. Highly motivated graduate
+          students will also be permitted to attend.
         </p>
       </div>
     </div>
--- src/components/DownloadPage/downloadContent.jsx
+++ src/components/DownloadPage/downloadContent.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
+import React from "react";
 
 export default function DownloadContent() {
-    return (
-        <div className='subhead-container'>
-
-            <div className="download-content">
-                <h1>TOPICS TO BE REVISED</h1>
-                <p>1. MATRICES, VECTOR SPACES<br />
-                    2. LINEAR TRANSFORMATION<br />
-                    3. EIGENVALUES & EIGENVECTORS<br />
-                    4. SPECIAL MATRICES<br />
-                    5. DECOMPOSITION<br />
-                    6. INNER PRODUCT SPACE<br />
-                    7. APPLICATIONS IN GRAPH<br />
-                    8. APPLICATIONS IN LINEAR COMPLEMENTARITY PROBLEMS<br />
-                    9. APPLICATIONS IN GAME THEORY
-                </p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="subhead-container">
+      <div className="download-content">
+        <h1>TOPICS TO BE COVERED</h1>
+        <p>
+          <ol>
+            <li>MATRICES, VECTOR SPACES</li>
+            <li>LINEAR TRANSFORMATION</li>
+            <li> EIGENVALUES & EIGENVECTORS</li>
+            <li>SPECIAL MATRICES & DECOMPOSITIONS</li>
+            <li>INNER PRODUCT SPACE</li>
+            <li>APPLICATIONS IN GRAPH</li>
+            <li>APPLICATIONS IN LINEAR COMPLEMENTARITY PROBLEMS</li>
+            <li>APPLICATIONS IN GAME THEORY</li>
+          </ol>
+        </p>
+      </div>
+    </div>
+  );
 }
