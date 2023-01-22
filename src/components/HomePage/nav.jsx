3a9384dbe763d40d49519fbc8195a36e25bf03f9
David Raj Micheal <daviddgl2013@gmail.com>
2023-01-22 20:44:23 +0530

    Update nav.jsx

--- src/components/HomePage/nav.jsx
+++ src/components/HomePage/nav.jsx
@@ -1,63 +1,103 @@
-import React from 'react';
-import { NavHashLink as NavLink } from 'react-router-hash-link';
-
-
-
-
-
+import React from "react";
+import { NavHashLink as NavLink } from "react-router-hash-link";
 
 export default function Nav() {
-    return (
+  return (
+    <div className="nav-container">
+      <nav>
+        <ul className="navList">
+          <li className="nav-items" id="Home">
+            <NavLink to={{ pathname: "/home", hash: "#HomePage" }}>
+              <button className="navButton">
+                <NavLink
+                  to={{ pathname: "/home", hash: "#HomePage" }}
+                  className="navButton navText"
+                >
+                  HOME
+                </NavLink>
+              </button>
+            </NavLink>
+          </li>
 
-        <div className='nav-container'>
+          <li className="nav-items" id="About">
+            <NavLink to={{ pathname: "/home", hash: "#AboutPage" }}>
+              <button className="navButton">
+                <NavLink
+                  to={{ pathname: "/home", hash: "#AboutPage" }}
+                  className="navButton navText"
+                >
+                  ABOUT
+                </NavLink>
+              </button>
+            </NavLink>
+          </li>
 
-            <nav>
-                <ul className='navList'>
-                    <li className='nav-items' id='Home'>
-                        <NavLink to={{ pathname: "/home", hash: "#HomePage" }} ><button className='navButton'><NavLink to={{ pathname: "/home", hash: "#HomePage" }} className="navButton navText">
-                            HOME
-                        </NavLink></button></NavLink></li>
+          <li className="nav-items" id="Speakers">
+            <NavLink to={{ pathname: "/home", hash: "#SpeakersPage" }}>
+              <button className="navButton">
+                <NavLink
+                  to={{ pathname: "/home", hash: "#SpeakersPage" }}
+                  className="navButton navText"
+                >
+                  SPEAKERS
+                </NavLink>
+              </button>
+            </NavLink>
+          </li>
 
-                    <li className='nav-items' id='About'>
-                        <NavLink to={{ pathname: "/home", hash: "#AboutPage" }}  ><button className='navButton'><NavLink to={{ pathname: "/home", hash: "#AboutPage" }} className="navButton navText" >
-                            ABOUT
-                        </NavLink></button></NavLink></li>
+          <li className="nav-items" id="Schedule">
+            <NavLink to={{ pathname: "/home", hash: "#SchedulePage" }}>
+              <button className="navButton">
+                <NavLink
+                  to={{ pathname: "/home", hash: "#SchedulePage" }}
+                  className="navButton navText"
+                >
+                  SCHEDULE
+                </NavLink>
+              </button>
+            </NavLink>
+          </li>
 
-                    <li className='nav-items' id='Speakers'>
-                        <NavLink to={{ pathname: "/home", hash: "#SpeakersPage" }}><button className='navButton'><NavLink to={{ pathname: "/home", hash: "#SpeakersPage" }} className="navButton navText">
-                            SPEAKERS
-                        </NavLink></button></NavLink></li>
+          <li className="nav-items" id="Download">
+            <NavLink to={{ pathname: "/home", hash: "#DownloadPage" }}>
+              <button className="navButton">
+                <NavLink
+                  to={{ pathname: "/home", hash: "#DownloadPage" }}
+                  className="navButton navText"
+                >
+                  DOWNLOAD
+                </NavLink>
+              </button>
+            </NavLink>
+          </li>
 
-                    <li className='nav-items' id='Schedule'>
-                        <NavLink to={{ pathname: "/home", hash: "#SchedulePage" }}><button className='navButton'><NavLink to={{ pathname: "/home", hash: "#SchedulePage" }} className="navButton navText">
-                            SCHEDULE
-                        </NavLink></button></NavLink></li>
-
-                    <li className='nav-items' id='Download'>
-                        <NavLink to={{ pathname: "/home", hash: "#DownloadPage" }}><button className='navButton'><NavLink to={{ pathname: "/home", hash: "#DownloadPage" }} className="navButton navText">
-                            DOWNLOAD
-                        </NavLink></button></NavLink></li>
-
-                    {/* just type in sponsor in navText class a to enable the sponsors button  */}
-                    <li className='nav-items part2' id='Sponsors'>
-                        <NavLink to="/team" ><button className='navButton'><NavLink to="/team" className="navButton navText">
-                            TEAM
-                        </NavLink></button></NavLink></li>
-                    {/* just type in sponsor in navText class a to enable the sponsors button  */}
-                    {/* <li className='nav-items' id='FAQs'>
+          {/* just type in sponsor in navText class a to enable the sponsors button  */}
+          <li className="nav-items part2" id="Sponsors">
+            <NavLink to="/team" className="navButton navText">
+              <button className="navButton">TEAM</button>
+            </NavLink>
+          </li>
+          {/* just type in sponsor in navText class a to enable the sponsors button  */}
+          {/* <li className='nav-items' id='FAQs'>
                         <NavLink to="/#FAQPage">
                             <button className='navButton'><NavLink to="#FAQPage" className="navButton navText">
 
                             </NavLink></button></NavLink></li> */}
 
-                    <li className='nav-items' id='Registration'>
-
-                        <NavLink to={{ pathname: "/home", hash: "RegistrationPage" }}><button className='navButton'><NavLink to={{ pathname: "/home", hash: "RegistrationPage" }} className="navButton navText">
-                            REGISTRATIONS
-                        </NavLink></button></NavLink></li>
-                </ul>
-            </nav>
-
-        </div >
-    );
+          <li className="nav-items" id="Registration">
+            <NavLink to={{ pathname: "/home", hash: "RegistrationPage" }}>
+              <button className="navButton">
+                <NavLink
+                  to={{ pathname: "/home", hash: "RegistrationPage" }}
+                  className="navButton navText"
+                >
+                  REGISTRATIONS
+                </NavLink>
+              </button>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
