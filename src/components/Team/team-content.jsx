3c11944e075b530c14e36dd68450008711142a5c
David Raj Micheal <daviddgl2013@gmail.com>
2023-02-13 01:41:12 +0530

    spelling corrections in team

--- src/components/Team/team-content.jsx
+++ src/components/Team/team-content.jsx
@@ -1,83 +1,66 @@
 import React from "react";
 
 export default function TeamContent() {
-    return (
-        <>
-
-
-            <div className="organizers">
-
-                <h1 className="team-heading">
-                    ORGANIZERS
-                </h1>
-                <p className="team-content">
-                    Dr. David Raj Micheal <br />
-                    Dr. P Sushmitha
-                </p>
-            </div>
-            <div className="Committee1">
-
-                <h1 className="team-heading">
-                    Registrations, Certificates and ID Cards
-                </h1>
-                <p className="team-content">
-                    <span id="color">Coordinators:</span> Dr. Manimaran<br />
-                    <span id="color">Members:</span> Dr. Radha and Dr. Dileswar Rao
-                </p>
-            </div>
-            <div className="Committee2">
-
-                <h1 className="team-heading">
-                    Food
-                </h1>
-                <p className="team-content">
-                    <span id="color">Coordinators:</span> Dr. Felix <br />
-                    <span id="color">Members: </span> Dr. Durgaprasad, Dr. Sudip Debnath and Dr. Dhivya P
-                </p>
-            </div>
-            <div className="Committee3">
-
-                <h1 className="team-heading">
-                    PR and Accommodation
-                </h1>
-                <p className="team-content">
-                    <span id="color">Coordinators:</span> Dr. Dhanasekar<br />
-                    <span id="color">Members: </span>Dr. G.K.Revathi , Dr. Sudip Debnath and Dr. David Maxim Gururaj
-                </p>
-            </div>
-            <div className="Committee4">
-
-                <h1 className="team-heading">
-                    Guest Care + Venue
-                </h1>
-                <p className="team-content">
-                    <span id="color">Coordinators:</span> Dr. Parthiban<br />
-                    <span id="color">Members:</span> Dr. M Kaliyappan and Dr. David Maxim Gururaj
-                </p>
-            </div>
-            <div className="Committee5">
-
-                <h1 className="team-heading">
-                    Technical
-                </h1>
-                <p className="team-content">
-                    <span id="color">Coordinators:</span> Dr. Lakshmanan<br />
-                    <span id="color">Members:</span> Dr. Sethukumarasamy, Dr. Balaji and Dr. Rajesh Kumar Mohapatra
-                </p>
-            </div>
-            <div className="Committee6">
-
-                <h1 className="team-heading">
-                    Finance
-                </h1>
-                <p className="team-content">
-                    <span id="color">Coordinators:</span> Dr. Manivannan<br />
-                    <span id="color">Members:</span> Dr. Nathiya, Dr Hannah Grace and Dr. Kamalesh AcharyA
-                </p>
-            </div>
-
-
-
-        </>
-    );
+  return (
+    <>
+      <div className="organizers">
+        <h1 className="team-heading">ORGANIZERS</h1>
+        <p className="team-content">
+          Dr. David Raj Micheal <br />
+          Dr. P Sushmitha
+        </p>
+      </div>
+      <div className="Committee1">
+        <h1 className="team-heading">
+          Registrations, Certificates and ID Cards
+        </h1>
+        <p className="team-content">
+          <span id="color">Coordinators:</span> Dr. Manimaran J<br />
+          <span id="color">Members:</span> Dr. Radha and Dr. Dileswar Rao
+        </p>
+      </div>
+      <div className="Committee2">
+        <h1 className="team-heading">Food</h1>
+        <p className="team-content">
+          <span id="color">Coordinators:</span> Dr. Felix A<br />
+          <span id="color">Members: </span> Dr. Durgaprasad, Dr. Sudip Debnath
+          and Dr. Dhivya P
+        </p>
+      </div>
+      <div className="Committee3">
+        <h1 className="team-heading">PR and Accommodation</h1>
+        <p className="team-content">
+          <span id="color">Coordinators:</span> Dr. Dhanasekar P<br />
+          <span id="color">Members: </span>Dr. G.K.Revathi , Dr. Sudip Debnath
+          and Dr. David Maxim Gururaj
+        </p>
+      </div>
+      <div className="Committee4">
+        <h1 className="team-heading">Guest Care + Venue</h1>
+        <p className="team-content">
+          <span id="color">Coordinators:</span> Dr. V. Parthiban
+          <br />
+          <span id="color">Members:</span> Dr. M Kaliyappan and Dr. David Maxim
+          Gururaj
+        </p>
+      </div>
+      <div className="Committee5">
+        <h1 className="team-heading">Technical</h1>
+        <p className="team-content">
+          <span id="color">Coordinators:</span> Dr. Lakshmanan
+          <br />
+          <span id="color">Members:</span> Dr. Sethukumarasamy, Dr. Balaji and
+          Dr. Rajesh Kumar Mohapatra
+        </p>
+      </div>
+      <div className="Committee6">
+        <h1 className="team-heading">Finance</h1>
+        <p className="team-content">
+          <span id="color">Coordinators:</span> Dr. Manivannan A<br />
+          <span id="color">Members:</span> Dr. Nathiya, Dr Hannah Grace and Dr.
+          Kamalesh Acharya
+        </p>
+      </div>
+    </>
+  );
 }
