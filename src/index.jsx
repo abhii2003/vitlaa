8eba7a0162c047e493ecde3656c20079e574b1f8
abhi <abhinavkush2003@gmail.com>
2023-02-13 11:31:02 +0530

    css corrected for computer screen after the addition of the registration fee details - abhi

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import "./computer.css";
 import "./mobile.css";
 import "./tablet.css";
-
+// import "./desktopmode.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<App />);
